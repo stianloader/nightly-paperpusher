08df2889328121408930162dba1e78f1aad5d745
Geolykt <admin@geolykt.de>
2025-08-26 13:31:23 +0200

    Implement classes listing

--- src/main/java/org/stianloader/paperpusher/search/SearchContext.java
+++ src/main/java/org/stianloader/paperpusher/search/SearchContext.java
@@ -121,6 +121,7 @@ public class SearchContext {
         server.post(finalPrefix + "/*", methodNotAllowedHandler);
         server.get(finalPrefix + "/projects", (ctx) -> DeltaServer.listProjects(this.searchDatabaseConnection, ctx));
         server.get(finalPrefix + "/packages/{projectid}", (ctx) -> DeltaServer.listPackages(this.searchDatabaseConnection, ctx));
+        server.get(finalPrefix + "/classes/{packageid}", (ctx) -> DeltaServer.listClasses(this.searchDatabaseConnection, ctx));
 
         publish.addPublicationListener(this::updateMavenIndex);
     }
